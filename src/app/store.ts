49d9db6e4907a6ef295ff872333d8030a4cf6754
Pieter <pieter@denieuwezaak.nl>
2025-01-28 08:45:10 +0100

    some small changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,12 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
 import counterReducer from "@/features/counter/counterSlice"
-import categoriesReducer from "@/features/categories/categoriesSlice"
+
 import cartReducer from "@/features/cart/cartSlice"
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    categories: categoriesReducer,
+   
     cart: cartReducer,
   },
 })
